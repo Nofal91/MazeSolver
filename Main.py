6b7a7c7661de96de1c4fd855869ebbe3e81d8a20
Walid Nofal <walid8789@gmail.com>
2026-05-04 19:47:46 +0300

    visuals edit

--- Main.py
+++ Main.py
@@ -1,17 +1,23 @@
 from pyamaze import maze, COLOR, agent, textLabel
-
+from timeit import timeit
 from Dfs import dfs
 from aStar import aStar
 
-m=maze(15,15)
+m=maze(10,10)
 #m.CreateMaze(theme=COLOR.light)
-m.CreateMaze(loopPercent=50,loadMaze='mainMaze.csv')
+x=1
+y=2
+goal=(x,y)
+m.CreateMaze(x,y,loopPercent=50)
+#,loadMaze='mainMaze.csv'
 # print(m.maze_map)
 textLabel(m, 'Optimal path', 'Yellow')
 textLabel(m, 'Optimal steps', len(m.path))
 l2 = textLabel(m, 'DFS search', 'Red')
 l3 = textLabel(m, 'DFS final path', 'Blue')
-A_fwdPath,A_exploredPath=aStar(m)
+
+
+A_fwdPath,A_exploredPath=aStar(m,goal)
 Dfs_fwdPath,Dfs_exploredPath=dfs(m)
 
 
@@ -32,7 +38,7 @@ mainPath=agent(m,footprints=True,color=COLOR.yellow)
 # A Star
 
 m.tracePath({mainPath:m.path},delay=150)
-m.tracePath({b:A_exploredPath},delay=100) #if the whole path needed
+# m.tracePath({b:A_exploredPath},delay=200) #if the whole path needed
 m.tracePath({a:A_fwdPath},delay=150)
 
 m.run()
--- aStar.py
+++ aStar.py
@@ -4,22 +4,22 @@ def h(cell1,cell2):
     x1,y1=cell1
     x2,y2=cell2
     return abs(x1-x2)+abs(y1-y2)
-def aStar(m):
+def aStar(m,g):
     start=(m.rows,m.cols)
     g_score = {cell: float('inf') for cell in m.grid}
     g_score[start] = 0
     f_score = {cell: float('inf') for cell in m.grid}
-    f_score[start] = h(start,(1,1))
+    f_score[start] = h(start,g)
 
     q = PriorityQueue()
 
-    q.put((h(start, (1, 1)), h(start, (1, 1)), start))
+    q.put((h(start, g), h(start, g), start))
     aPath = {}
     aExplored = []
     while not q.empty():
         current = q.get()[2]
         aExplored.append(current)
-        if current==(1,1):
+        if current==g:
             break
         for d in 'ESNW':
             if m.maze_map[current][d]==True:
@@ -32,15 +32,15 @@ def aStar(m):
                 elif d == 'S':
                     child=(current[0]+1,current[1])
                 new_g_score = g_score[current] + 1
-                new_f_score = new_g_score + h(child, (1, 1))
+                new_f_score = new_g_score + h(child, g)
                 if new_f_score < f_score[child]:
                     f_score[child] = new_f_score
                     g_score[child] = new_g_score
-                    q.put((new_f_score, h(child,(1,1)), child))
+                    q.put((new_f_score, h(child,g), child))
                     aPath[child]=current
     l4 = textLabel(m, 'A* steps', len(aExplored) - 1)
     fwdPath = {}
-    goal = (1, 1)
+    goal = g
     while goal != start:
         fwdPath[aPath[goal]]=goal
         goal=aPath[goal]
