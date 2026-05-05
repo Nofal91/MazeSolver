0a3d3c8e368645ef09e4cdb983049fd554e4c812
Walid Nofal <walid8789@gmail.com>
2026-05-05 09:37:11 +0300

    minor edits

--- Main.py
+++ Main.py
@@ -18,7 +18,7 @@ m=maze(k,l)
 goal=(goalX,goalY)
 
 if randMaze:
-    m.CreateMaze(goal[0],goal[1], loopPercent=50)
+    m.CreateMaze(goal[0],goal[1], loopPercent=55)
 else:
     m.CreateMaze(goal[0],goal[1], loadMaze='mainMaze.csv')
     
@@ -26,10 +26,10 @@ else:
 
 # print(m.maze_map)
 
-textLabel(m, 'Optimal path', 'Yellow')
+# textLabel(m, 'Optimal path', 'Yellow')
 textLabel(m, 'Optimal steps', len(m.path))
-l2 = textLabel(m, 'search', 'Red')
-l3 = textLabel(m, 'final path', 'Blue')
+# l2 = textLabel(m, 'search', 'Red')
+# l3 = textLabel(m, 'final path', 'Blue')
 
 
 
@@ -43,26 +43,31 @@ BFS_fwdpath, BFS_exploredPath = BFS(m, goal)
 
 a = agent(m, footprints=True, filled=True)
 b = agent(m, footprints=True, color=COLOR.red, shape='arrow')
-mainPath = agent(m, footprints=True, color=COLOR.yellow)
+bf = agent(m, footprints=True, color=COLOR.red, filled=True)
+mainPath = agent(m, footprints=True, color=COLOR.yellow,filled=True)
 
 
 m.tracePath({mainPath:m.path}, delay=150)
 
 if algorithm == "DFS": ##  DFS
 
-    if searchPath : m.tracePath({b:DFS_exploredPath}, delay=200) #if the whole path needed
+    if searchPath : m.tracePath({b:DFS_exploredPath}, delay=120) #if the whole path needed
     m.tracePath({a:DFS_fwdPath}, delay=150)
 
 elif algorithm == "BFS": ## BFS
 
-    if searchPath : m.tracePath({b:BFS_exploredPath}, delay=200) #if the whole path needed
-    m.tracePath({a:BFS_fwdpath},delay=150)
+    if searchPath : m.tracePath({bf:BFS_exploredPath}, delay=40) #if the whole path needed
+    m.tracePath({a:BFS_fwdpath},delay=130)
 
 elif algorithm == "A*": ## A*
 
-    if searchPath : m.tracePath({b:A_exploredPath},delay=200) #if the whole path needed
+    if searchPath : m.tracePath({b:A_exploredPath},delay=120) #if the whole path needed
     m.tracePath({a:A_fwdPath},delay=150)
 
 
-
+m._win.attributes('-topmost', True)
+m._win.update()
+m._win.attributes('-topmost', False)
+m._win.lift()
+m._win.focus_force()
 m.run()
